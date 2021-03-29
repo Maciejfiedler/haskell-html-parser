bd520019eae737debaabca0eed576c300bfbf108
Maciej Fiedler <maciekfiedler@gmail.com>
2021-03-29 15:26:54 +0200

    changd bytestring operations to string operations

--- Main.hs
+++ Main.hs
@@ -1,8 +1,7 @@
 import Network.HTTP.Simple
-import qualified Data.ByteString.Char8 as BS
 
 request = parseRequest_ "https://google.com"
 
 response = httpBS request
 
-main = response >>= BS.putStrLn . getResponseBody
+main = response >>= putStrLn . show
