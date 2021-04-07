ec116adebcdc86bce2eb2040c5c20bdac7ea6a44
Maciej Fiedler <maciekfiedler@gmail.com>
2021-04-07 17:27:10 +0200

    added link parser -- NOT COMPLETE

--- Main.hs
+++ Main.hs
@@ -1,7 +1,31 @@
 import Network.HTTP.Simple
+import Text.Parsec
+import Text.Parsec.Char
+import Text.Parsec.Combinator
+import Text.Parsec.ByteString
+import qualified Text.Parsec.Token as T
 
-request = parseRequest_ "https://google.com"
-
+url = "https://haskell.org"
+request = parseRequest_ url
 response = httpBS request
+responseInString = response >>= putStrLn . show
 
-main = response >>= putStrLn . show
+-- Expressions to parse
+data HTMLExpr = Link String
+                deriving (Eq, Show)
+
+parseHTMLLink :: Parser HTMLExpr
+parseHTMLLink = do
+    skipMany1 notLink
+    insideA = between (T.symbol "<a>") (T.symbol "</a>") 
+    -- go to href
+    void $ insideA $ T.lexeme $ string "href=\""
+    -- get link
+    parseLink <- link
+    -- close href
+    void $ char '"'
+
+
+  where 
+    notLink = satisfy (\a -> /= "<a>")
+    link = satisfy (\a -> /= '"')
